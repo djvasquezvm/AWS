10450c1daf0376dc0361af8d673b4752dfa412b9
daniel <djvasquezvm@gmail.com>
2021-09-19 14:13:07 -0500

    first commit

---
+++ SQS_JS-SDK/ListQueues/sqs_listqueues.js
@@ -0,0 +1,13 @@
+import { ListQueuesCommand } from  "@aws-sdk/client-sqs";
+import { sqsClient } from  "../libs/sqsClient.js";
+
+const run = async () => {
+  try {
+    const data = await sqsClient.send(new ListQueuesCommand({}));
+    console.log("Success", data);
+    return data;
+  } catch (err) {
+    console.error(err, err.stack);
+  }
+};
+run();
